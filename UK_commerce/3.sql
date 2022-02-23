91ddf95708730e081bfa66c08c9342b4ccc7be7a
onejinee <wonjin1008@naver.com>
2022-02-23 15:06:13 +0900

    Update 3.sql

--- UK_commerce/3.sql
+++ UK_commerce/3.sql
@@ -1,14 +1,14 @@
 SELECT a.Country, 
-	   year(a.InvoiceDate) as Y, 
+       year(a.InvoiceDate) as Y, 
        count(DISTINCT b.CustomerID)/count(DISTINCT a.CustomerID) as RetentionRate
 FROM (
 	SELECT DISTINCT Country, InvoiceDate, CustomerID
 	FROM data.uk_commerce
-    ) a
+) a
 LEFT JOIN (
 	SELECT DISTINCT Country, InvoiceDate, CustomerID
-    FROM data.uk_commerce
-    ) b
+        FROM data.uk_commerce
+) b
 ON year(a.InvoiceDate) = year(b.InvoiceDate) - 1
 AND a.Country = b.Country
 AND a.CustomerID = b.CustomerID
