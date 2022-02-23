6ea3468318424c3128e63cba43a51da97cb64b17
onejinee <wonjin1008@naver.com>
2022-02-23 14:54:34 +0900

    revenue by country and stock

--- UK_commerce/first.sql
+++ UK_commerce/first.sql
@@ -1,7 +1,6 @@
--- Q1 
 SELECT Country, 
 	   StockCode, 
        count(DISTINCT CustomerID) as BU, 
        sum(Quantity*UnitPrice) as Total
-FROM challenge_final.final
+FROM data.uk_commerce
 GROUP BY 1, 2;
