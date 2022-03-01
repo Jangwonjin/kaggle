a3e945c1e74b87b3bc5f8e0de11c2cd1c14efe24
onejinee <wonjin1008@naver.com>
2022-03-01 17:46:32 +0900

    upload 4th code

---
+++ UK_commerce/4.sql
@@ -0,0 +1,9 @@
+SELECT t.Country, 
+       sum(CASE WHEN cnt = 1 THEN 1 ELSE 0 END) / sum(1) as BounceRate
+FROM (
+	SELECT Country, CustomerID, count(DISTINCT InvoiceDate) as cnt
+	FROM challenge_final.final
+	GROUP BY 1, 2
+) t
+GROUP BY 1
+ORDER BY 1;
