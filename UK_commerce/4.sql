28df5d7c1cfc43bd7a9b556417deb99107faac1f
onejinee <wonjin1008@naver.com>
2022-03-09 15:09:47 +0900

    Delete 4.sql

--- UK_commerce/4.sql
+++
@@ -1,9 +0,0 @@
-SELECT t.Country, 
-       sum(CASE WHEN cnt = 1 THEN 1 ELSE 0 END) / sum(1) as BounceRate
-FROM (
-	SELECT Country, CustomerID, count(DISTINCT InvoiceDate) as cnt
-	FROM data.uk_commerce
-	GROUP BY 1, 2
-) t
-GROUP BY 1
-ORDER BY 1;
